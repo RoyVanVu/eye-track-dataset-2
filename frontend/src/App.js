944d3bb4de33960c5cc5b3297d2c1e010ea11cc9
RoyVanVu <132107942+RoyVanVu@users.noreply.github.com>
2025-09-15 01:22:48 -0700

    facial landmark with facemesh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,47 @@ import * as tf from "@tensorflow/tfjs";
 import * as facemesh from "@tensorflow-models/facemesh";
 import Webcam from "react-webcam";
 import './App.css';
+import { drawMesh } from './utilities';
 
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+
+  const runFacemesh = async () => {
+    const net = await facemesh.load({
+      inputResolution: {width:640, height:480}, 
+      scale:0.8
+    });
+    setInterval(() => {
+      detect(net)
+    }, 100)
+  };
+
+  const detect =  async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" && 
+      webcamRef.current !== null && 
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const face = await net.estimateFaces(video);
+      console.log(face);
+
+      const ctx = canvasRef.current.getContext("2d");
+      drawMesh(face, ctx);
+    }
+  };
+
+  runFacemesh();
 
   return (
     <div className="App">
