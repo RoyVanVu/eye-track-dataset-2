1cad222bef24fdd639d27a2a2776bbcb88e98614
RoyVanVu <132107942+RoyVanVu@users.noreply.github.com>
2025-09-14 23:57:52 -0700

    initial set up

---
+++ frontend/src/App.js
@@ -0,0 +1,47 @@
+import React, {useRef} from 'react';
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/facemesh";
+import Webcam from "react-webcam";
+import './App.css';
+
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position:"absolute",
+            marginLeft:"auto",
+            marginRight:"auto",
+            left:0,
+            right:0,
+            textAlign:"center",
+            zIndex:9,
+            width:640,
+            height:480
+          }}
+        />
+        <canvas 
+          ref={canvasRef}
+          style={{
+            position:"absolute",
+            marginLeft:"auto",
+            marginRight:"auto",
+            left:0,
+            right:0,
+            textAlign:"center",
+            zIndex:9,
+            width:640,
+            height:480
+          }}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
